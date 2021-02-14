ca3dd2ed72fb8c6452d3afd4de47465ab08f2332
feinoko <feinoko@gmail.com>
2021-02-14 19:51:09 +0100

    markup progress

---
+++ src/app.js
@@ -0,0 +1,4 @@
+// import {hey} from './js/bro'; // example to show routing
+import 'regenerator-runtime/runtime' // required when using async/await with parcel
+import './css/styles.scss'; // styles entry point
+
