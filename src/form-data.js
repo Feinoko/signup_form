d4f5a196713d907113ccd30809c24a353333631b
feinoko <feinoko@gmail.com>
2021-02-26 19:23:47 +0100

    MVP ok

--- src/form-data.js
+++ src/form-data.js
@@ -2,6 +2,8 @@
 import 'regenerator-runtime/runtime'; // required when using async/await with parcel
 import './css/styles.scss'; // styles entry point
 
+
+// displaying form data
 const formData_EL = document.querySelector('.form-data');
 new URLSearchParams(window.location.search).forEach((value, key) => {
   formData_EL.append(`${key}: ${value}`);
